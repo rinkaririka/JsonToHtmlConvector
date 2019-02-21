adbf052e94665ebade4e807ca3a24f20c2491dbe
Борисов Максим <shiftleft@inbox.ru>
2019-02-21 20:07:25 +0300

    Task 3

--- converter.py
+++ converter.py
@@ -2,6 +2,21 @@ import conf
 import json
 
 
+def ul_decoration(function_to_decorate):
+    def wrapper(element, arg):
+        result = "<ul>"
+        result += function_to_decorate(element, arg)
+        result += "</ul>"
+        return result
+    return wrapper
+
+
+def li_decoration(function_to_decorate):
+    def wrapper(block):
+        return "<li>" + function_to_decorate(block) + "</li>"
+    return wrapper
+
+
 class ConverterJsonToHtml():
 
     def _convert_json_to_python_object(self):
@@ -36,6 +51,18 @@ class ConverterJsonToHtml():
         input_file.write(html_code)
         input_file.close()
 
+    def _assembling(self, blocks, generation):
+        """
+        Processing each block in a certain way.
+        :param blocks: list of dictionary with tag and body
+        :param generation: function for processing block
+        :return: finished result after processing all blocks (finished string with html code in this project)
+        """
+        result = ""
+        for block in blocks:
+            result += generation(block)
+        return result
+
     def _parsing_python_object(self, python_object):
         """
         Parses input and selects conversion based on type.
@@ -43,15 +70,20 @@ class ConverterJsonToHtml():
         :return: string with finished html code
         """
 
-        result = ""
+        generation_function = None
+        assembling_function = self._assembling
+        block_to_generate = []
         if type(python_object) is list:
-            for block in python_object:
-                result += self._generation_html_block(block)
+            block_to_generate = python_object
+            generation_function = li_decoration(self._generation_html_block)
+            assembling_function = ul_decoration(self._assembling)
+
         elif type(python_object) is dict:
-            result += self._generation_html_block(python_object)
+            block_to_generate = [python_object]
+            generation_function = self._generation_html_block
         else:
             raise TypeError
-        return result
+        return assembling_function(block_to_generate, generation_function)
 
     def convert_json_to_html(self):
         """
